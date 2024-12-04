14443f3200331e21986f60e4abb68f8cbbe63a46
JBurton175 <jburton175@gmail.com>
2024-12-04 08:30:10 -0700

    changed cookies back to false

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(session({
     secret: 'intex2346235346', // Replace with a strong secret key
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true } // Use `true` for HTTPS
+    cookie: { secure: false } // Use `true` for HTTPS
 }));
 
 const knex = require("knex")({
