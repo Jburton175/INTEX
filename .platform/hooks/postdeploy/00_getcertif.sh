e81a5507bf7f895ce3010680640ef86697d21e6e
JBurton175 <jburton175@gmail.com>
2024-12-05 20:16:13 -0700

    section1group9intex.is404.net

--- .platform/hooks/postdeploy/00_getcertif.sh
+++ .platform/hooks/postdeploy/00_getcertif.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo certbot -n -d turtleshelter.josh-burton-bi.com  --nginx --agree-tos --email jnburton0@gmail.com
+sudo certbot -n -d section1group9intex.is404.net  --nginx --agree-tos --email jburton175@gmail.com
