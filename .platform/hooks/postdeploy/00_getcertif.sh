63650dff2e8a2e5a606f8501072c9bff727e49bf
JBurton175 <jburton175@gmail.com>
2024-12-05 19:59:19 -0700

    turtleshelter.josh-burton-bi.com

--- .platform/hooks/postdeploy/00_getcertif.sh
+++ .platform/hooks/postdeploy/00_getcertif.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo certbot -n -d josh-burton-bi.com  --nginx --agree-tos --email jnburton0@gmail.com
+sudo certbot -n -d turtleshelter.josh-burton-bi.com  --nginx --agree-tos --email jnburton0@gmail.com
